f4f6653d41c8aea75e54fc6724173e3b363dd175
xsavqq <xsavqq@gmail.com>
2026-09-15 10:52:04 +0300

    Update states.py

--- states.py
+++ states.py
@@ -2,11 +2,11 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class OrderStates(StatesGroup):
-    waiting_link = State()      # ссылка на товар / фото / "-"
+    waiting_link = State()      # ссылка или "-"
+    waiting_photo = State()     # фото / скрин товара
     waiting_price = State()     # цена в юанях
     waiting_qty = State()       # количество
     waiting_size = State()      # размер или "-"
-    waiting_weight = State()    # вес товара в кг
 
 
 class CabinetStates(StatesGroup):
